aeae30cb12165d3985419374751d7ff5f13a3301
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-02-25 23:07:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class Student:
+  def __init__(self, age, weight, height, first_name, last_name, catch_phrase):
+    self.age = age
+    self.weight = weight
+    self.height = height
+    self.first_name = first_name
+    self.last_name = last_name
+    self.catch_phrase = catch_phrase
+
+  def 
